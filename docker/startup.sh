451e5ca1b34598ff98e6d7bf0ea2cb936eb41fcb
kkaris <karis.klas@gmail.com>
2026-01-28 15:34:46 -0800

    Build resources at Docker build time

---
+++ docker/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+GILDA_VERSION=$(python -c "import gilda; print(gilda.__version__)")
+export GILDA_TERMS="/root/.data/gilda/${GILDA_VERSION}/grounding_terms.db"
+
+gunicorn indra_agent.mcp_server.server:app --bind 0.0.0.0:8778 --w 4 --worker-class uvicorn.workers.UvicornWorker
